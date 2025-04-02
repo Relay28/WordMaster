609919e90fd423e41a032ca3a3146004f9202e72
Rae Duqs <raeaddison.duque@cit.edu>
2025-04-02 21:09:15 +0800

    Backend Initialized

---
+++ backend/wrdmstr/src/main/java/cit/edu/wrdmstr/WrdmstrApplication.java
@@ -0,0 +1,13 @@
+package cit.edu.wrdmstr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WrdmstrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WrdmstrApplication.class, args);
+	}
+
+}
---
+++ backend/wrdmstr/src/test/java/cit/edu/wrdmstr/WrdmstrApplicationTests.java
@@ -0,0 +1,13 @@
+package cit.edu.wrdmstr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WrdmstrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
