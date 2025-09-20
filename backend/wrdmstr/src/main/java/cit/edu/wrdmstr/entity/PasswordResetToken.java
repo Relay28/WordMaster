e89ce23bc5c53faa36f5ca5deb4c8fb9f2214887
Relay28 <98829411+Relay28@users.noreply.github.com>
2025-09-20 23:28:14 +0800

    Add getters and setters to PasswordResetToken entity (Kay di mugana sa akoa ang Lombok)
    Introduced standard getter and setter methods for all fields in the PasswordResetToken class to improve encapsulation and facilitate access to entity properties.

--- backend/wrdmstr/src/main/java/cit/edu/wrdmstr/entity/PasswordResetToken.java
+++ backend/wrdmstr/src/main/java/cit/edu/wrdmstr/entity/PasswordResetToken.java
@@ -28,6 +28,38 @@ public class PasswordResetToken {
 
     private LocalDateTime expiryDate;
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+    public UserEntity getUser() {
+        return user;
+    }
+
+    public void setUser(UserEntity user) {
+        this.user = user;
+    }
+
+    public LocalDateTime getExpiryDate() {
+        return expiryDate;
+    }
+
+    public void setExpiryDate(LocalDateTime expiryDate) {
+        this.expiryDate = expiryDate;
+    }
+
     public boolean isExpired() {
         return LocalDateTime.now().isAfter(expiryDate);
     }
